3185f56c11e926275df0b3ab5fa5bfbcd3208f67
Jake Kohl <jacob.jp.kohl@gmail.com>
2025-11-25 15:28:20 -0600

    feature: cypress tests for new github related apis and home spec

--- frontend/cypress/e2e/api.cy.js
+++ frontend/cypress/e2e/api.cy.js
@@ -1,44 +1,56 @@
 describe('API Tests', () => {
-
   const apiUrl = Cypress.env('API_URL') || 'http://localhost:8000';
 
   it('GET /health should return a 200 status code and a message indicating the API is running', () => {
     cy.request('GET', `${apiUrl}/health`).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.be.an('object')
-      expect(response.body).to.have.property('status').to.eq('ok')
-      expect(response.body).to.have.property('message').to.eq('API is running')
-      expect(response.body).to.have.property('version').to.be.a('string')
-      expect(response.body).to.have.property('timestamp').to.be.a('string')
-    })
-  })
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('status').to.eq('ok');
+      expect(response.body).to.have.property('message').to.eq('API is running');
+      expect(response.body).to.have.property('version').to.be.a('string');
+      expect(response.body).to.have.property('timestamp').to.be.a('string');
+    });
+  });
 
   it('GET /me should return a 200 status code and a message indicating the API is running', () => {
     cy.request('GET', `${apiUrl}/me`).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.be.an('object')
-      expect(response.body).to.have.property('name').to.be.a('string')
-      expect(response.body).to.have.property('experiences').to.be.an('array')
-      expect(response.body.experiences.length).to.be.greaterThan(0)
-    })
-  })
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('name').to.be.a('string');
+      expect(response.body).to.have.property('experiences').to.be.an('array');
+      expect(response.body.experiences.length).to.be.greaterThan(0);
+    });
+  });
 
   it('GET /projects should return a 200 status code and a list of projects', () => {
     cy.request('GET', `${apiUrl}/projects`).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.be.an('array')
-      expect(response.body.length).to.be.greaterThan(0)
-    })
-  })
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('array');
+      expect(response.body.length).to.be.greaterThan(0);
+    });
+  });
 
   it('GET /contact should return a 200 status code for the contact page', () => {
     cy.request('GET', `${apiUrl}/contact`).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.be.an('object')
-      expect(response.body).to.have.property('contact').to.be.an('Array')
-      expect(response.body.contact.length).to.be.greaterThan(0)
-      expect(response.body.specialties).to.be.an('Array')
-      expect(response.body.specialties.length).to.be.greaterThan(0)
-    })
-  })
-})
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('contact').to.be.an('Array');
+      expect(response.body.contact.length).to.be.greaterThan(0);
+      expect(response.body.specialties).to.be.an('Array');
+      expect(response.body.specialties.length).to.be.greaterThan(0);
+    });
+  });
+
+  it('GET /github-stats should return a 200 status code and a list of github stats', () => {
+    cy.request('GET', `${apiUrl}/github-stats?year=2024`).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('totalContributions').to.be.a('number');
+      expect(response.body).to.have.property('year').to.be.a('string');
+      expect(response.body).to.have.property('contributions').to.be.an('array');
+      expect(response.body.contributions.length).to.be.greaterThan(0);
+      expect(response.body.username).to.be.a('string').to.equal('jakekohl');
+      expect(response.body.lastUpdated).to.be.a('string').to.not.be.eq(null);
+    });
+  });
+});
