d0f7aa71fbd77a6ec8a4508c304593a35fe6bb50
Jacob Kohl <jacob.jp.kohl@gmail.com>
2025-11-25 23:27:42 -0600

    role schema verification in Roles Card

--- frontend/cypress/support/commands/roles.js
+++ frontend/cypress/support/commands/roles.js
@@ -6,6 +6,14 @@
  */
 Cypress.Commands.add('validateRoles', (roles) => {
   roles.forEach(role => {
-    cy.getDataTest(role).should('be.visible');
+    cy.getDataTest(role).should('be.visible').within(() => {
+      cy.getDataTest('role-title').should('be.visible');
+      cy.getDataTest('role-company').should('be.visible');
+      cy.getDataTest('role-status').should('be.visible');
+      cy.getDataTest('role-location').should('be.visible');
+      cy.getDataTest('role-dates').should('be.visible');
+      cy.getDataTest('role-description').should('be.visible');
+      cy.getDataTest('role-url-button').should('be.visible');
+    });
   });
 });
