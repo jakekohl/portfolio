7983307fb7fb5f26b3978363945da238da7c16d9
Jake Kohl <jacob.jp.kohl@gmail.com>
2025-12-04 16:19:15 -0600

    feature: added entity object/querying to projects API

--- backend/items/projects.py
+++ backend/items/projects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Query
 from pydantic import BaseModel
 from lib.projects_service import ProjectsService
 from typing import Optional, Dict
@@ -7,20 +7,28 @@ router = APIRouter(tags=["projects"])
 
 class ProjectResponse(BaseModel):
   title: str
+  entity: Optional[str] = None
   description: str
   technologies: list[str]
   skillsLeveraged: list[str]
   status: str
-  github: str
+  github: Optional[str] = None
   demo: Optional[str] = None
   features: list[str]
   dataTest: str
-  images: list[Dict[str, str]]
+  images: list[Dict[str, str]] = []
 
 @router.get("/projects", response_model=list[ProjectResponse])
-async def get_projects():
+async def get_projects(
+  entity: Optional[str] = Query(None, description="Filter results based on associated entity (Company, Individual, etc). If not provided, returns all projects.")
+):
+  """Retrieves stored projects
+
+  Query Parameters:
+    - entity: Optional Entity Filter ('Self', 'Tagboard', etc). Returns only projects associated to that entity. If omitted, returns all projects.
+  """
   try:
-    projects = ProjectsService().get_projects()
+    projects = ProjectsService().get_projects(entity=entity)
     return projects
   except Exception as e:
     raise HTTPException(
--- backend/lib/projects_service.py
+++ backend/lib/projects_service.py
@@ -1,3 +1,4 @@
+from typing import Optional, Dict
 import util.db
 from pymongo.errors import ServerSelectionTimeoutError, ConnectionFailure, PyMongoError
 
@@ -6,10 +7,9 @@ class ProjectsService:
     collection_name = "projects"
     self.collection = util.db.get_collection(collection_name)
 
-  def get_projects(self):
+  def get_projects(self, entity: Optional[str] = None ) -> list[Dict]:
     try:
-      return [util.db.convert_objectid_to_str(project) for project in self.collection.find()]
+      return [util.db.convert_objectid_to_str(project) for project in self.collection.find({"entity": entity if entity else {"$exists": True}})]
     except (ServerSelectionTimeoutError, ConnectionFailure, PyMongoError) as e:
       print(f"MongoDB connection error in get_projects: {str(e)}")
-      # Return empty list instead of crashing
       return []
